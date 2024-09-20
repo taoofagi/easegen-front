a69b59d0e147b57ce2b4a56923b739380431a2d6
haojing <364430879@qq.com>
2024-09-20 12:00:57 +0800

    upgrade: 1、升级

--- vite.config.ts
+++ vite.config.ts
@@ -61,18 +61,6 @@ export default ({command, mode}: ConfigEnv): UserConfig => {
                 }
             ]
         },
-      module: {
-        rules: [
-          // 配置读取 *.md 文件的规则
-          {
-            test: /\.md$/,
-            use: [
-              { loader: "html-loader" },
-              { loader: "markdown-loader", options: {} }
-            ]
-          }
-        ]
-      },
         build: {
             minify: 'terser',
             outDir: env.VITE_OUT_DIR || 'dist',
